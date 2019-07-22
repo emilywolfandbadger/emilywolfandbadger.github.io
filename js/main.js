a59c7edf5ffae17395be59753e56f1755626a3d6
Emily Cuthbert <emily@Emilys-MacBook-Air.local>
2019-07-22 17:12:27 +0100

    Changing to Tom's resturant

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function initMap(){
+
+	var map = new google.maps.Map(document.getElementById("map"), {
+
+		center: {lat: 40.8054491, lng: -73.9654415},
+		zoom: 10
+	});
+
+	var market = new google.maps.Market({
+		position: {lat: 40.8054491, lng: -73.9654415},
+		map: map,
+		title: "Location"
+	});
+}
