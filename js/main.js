72930456757764e8dacaf01d4df0569f92d9bdfd
Emily Cuthbert <emily@emilys-air.home>
2019-07-22 19:56:52 +0100

    Update firebase

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var database = firebase.database();
+
 function initMap(){
 
 	var map = new google.maps.Map(document.getElementById("map"), {
@@ -12,3 +14,5 @@ function initMap(){
 		title: "Location"
 	});
 }
+
+var reservationData = {};
