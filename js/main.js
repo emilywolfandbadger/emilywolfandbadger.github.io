73b8f2c249192229fd1e087fe3bfa5001fe5af6a
Emily Cuthbert <emily@Emilys-MacBook-Air.local>
2019-07-30 23:27:31 +0100

    fix errors

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ function getReservations(){
 
 };
 
-$("document").ready(){
+$("document").ready(function(){
 	
 	var source = $("#reservation-template").html();
 	var template = Handlebars.compile(source);
@@ -66,7 +66,7 @@ $("document").ready(){
 	
 	$("#resv_list").append(newReservationHTML);			
 
-};
+});
 
 
 
